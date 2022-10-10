ae45bec2f16b2d25e7a3e7f8cc3e3ced19322120
Bennyhparh <74233673+Bennyhp@users.noreply.github.com>
2022-10-10 14:05:30 +0800

    Add files via upload

---
+++ PWEB B20/Pertemuan 3/javascript.js
@@ -0,0 +1,48 @@
+// console.log("Inside JavaScript");
+
+// alert("Website Berhasil Di Muat");
+
+// var yakin = confirm("Apakah anda yakin");
+// if (yakin == true) {
+//     console.log('Selamat Anda Yakin');
+//     var nama = prompt("Masukkan Nama Anda: ");
+//     console.log(nama);
+// } else {
+//     console.log("Anda Tidak Yakin");
+// }
+
+// const dom1 = document.getElementById("judul");
+// dom1.innerHTML = "PERTEMUAN 3";
+// dom1.style.color = "red";
+
+// const dom2 = document.getElementsByTagName("p");
+// dom2[0].style.color = "blue";
+// for(let i = 0; i<dom2.length; i++){
+//     dom2[i].style.color = "blue";
+// }
+
+// dom3 = document.getElementsByClassName("item");
+// dom3[0].style.color = "red";
+
+// salah ini
+// function press(){
+//     const ul = document.querySelector('section#query-slctr ul');
+
+//     const li = document.createElement('li');
+//     const item = document.createTextNode('item 4')
+
+//     li.appendChild(item);
+//     ul.appendChild(li);
+// };
+
+//ini bener
+// const press = document.getElementById("btn-info");
+// press.addEventListener("click", function showInfo() {
+//     const x = document.getElementById("info");
+//     if(x.style.display == "none"){
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// });
+
---
+++ PWEB B20/Pertemuan 4/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label for="username">Username</label><br>
+        <input type="text" name="regisUsername" id="username">
+
+        <label for="password">Password</label><br>
+        <input type="password" name="regisPassword" id="password">
+
+        <label for="password">Konfirmasi Password</label><br>
+        <input type="submit" name="regisSubmit" value="submit">
+    </form>
+</body>
+</html>
